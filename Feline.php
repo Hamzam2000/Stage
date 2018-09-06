fe2dc7fa7cb2d4bb36ac3eb75599842bbf448b43
Hamza <hamza.games2000@hotmail.com>
2018-09-06 12:08:43 +0200

    Alles uitelkaar gehaald

--- Feline.php
+++ Feline.php
@@ -14,28 +14,3 @@ class Lion extends Animal implements Mammal, Feline
         return 'I am chasing a Gazelle!';
     }
 }
-
-class Client
-{
-    public function __construct()
-    {
-        $dog = new Dog('Sparky', Mammal::GENDER_MALE);
-        $cat = new Lion;
-        $fish = new Fish;
-
-        $this->setupFeline($cat);
-        $this->setupCanine($dog);
-
-        $this->setupAnimal($cat);
-        $this->setupAnimal($dog);
-        $this->setupAnimal($fish);
-    }
-
-    public function setupFeline(Feline $cat)
-    {
-        echo 'Feline: ' . $this->iAmA($cat), ' and ',$cat->chase(), ' ', $cat->eat() . '<br>';
-    }
-
-}
-
-$client = new Client; ?>
