244bd531333326682192f389adbcb18222984ee4
Hamza <hamza.games2000@hotmail.com>
2018-09-06 12:09:44 +0200

    alles uitelkaar gehaald 2.0

--- Animal.php
+++ Animal.php
@@ -8,47 +8,6 @@ abstract class Animal
     }
 }
 
-class Dog extends Animal implements Canine, Mammal
-{
-    private $name;
-    private $gender;
-
-    public function __construct($name, $gender)
-    {
-        $this->setName($name);
-        $this->setGender($gender);
-    }
-
-    private function setName($name)
-    {
-        $this->name = (string) $name;
-    }
-
-    private function setGender($gender)
-    {
-        if ($gender === self::GENDER_MALE || $gender === self::GENDER_FEMALE) {
-            $this->gender = $gender;
-        } else {
-            throw new InvalidArgumentException('Mammals can only be male or female');
-        }
-    }
-
-    public function getGender()
-    {
-        return $this->gender();
-    }
-
-    public function isMale()
-    {
-        return $this->gender === self::GENDER_MALE;
-    }
-
-    public function isFemale()
-    {
-        return $this->gender === self::GENDER_FEMALE;
-    }
-}
-
 class Lion extends Animal implements Mammal, Feline
 {
     private $gender = self::GENDER_MALE;
@@ -63,41 +22,4 @@ class Lion extends Animal implements Mammal, Feline
         return $this->gender === self::GENDER_FEMALE;
     }
 
-}
-
-class Fish extends Animal
-{
-    final public function breath()
-    {
-        return 'I breath oxygen found in the water using my gills.';
-    }
-}
-
-class Client
-{
-    public function __construct()
-    {
-        $dog = new Dog('Sparky', Mammal::GENDER_MALE);
-        $cat = new Lion;
-        $fish = new Fish;
-
-        $this->setupFeline($cat);
-        $this->setupCanine($dog);
-
-        $this->setupAnimal($cat);
-        $this->setupAnimal($dog);
-        $this->setupAnimal($fish);
-    }
-
-    public function setupAnimal(Animal $animal)
-    {
-        echo $this->iAmA($animal) . ' and ' . $animal->breath() . '<br>';
-    }
-
-    private function iAmA(Animal $animal)
-    {
-        return 'I am a ' . get_class($animal);
-    }
-}
-
-$client = new Client; ?>
+} ?>
